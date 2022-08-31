bcdda29f0df3b17b0d24f821c5b2a6996a9c06b4
Maxim <25-nv-86@mail.ru>
2022-08-31 21:40:43 +0300

    Добавил модуль AppModule

--- app/src/main/java/com/maximmesh/movies/data/network/ApiService.kt
+++ app/src/main/java/com/maximmesh/movies/data/network/ApiService.kt
@@ -2,7 +2,10 @@ package com.maximmesh.movies.data.network
 
 import com.maximmesh.movies.data.models.Movies
 import retrofit2.Response
+import retrofit2.http.GET
 
 interface ApiService {
+
+    @GET("/shows")
     suspend fun getAllMovies() : Response<List<Movies>>
 }
